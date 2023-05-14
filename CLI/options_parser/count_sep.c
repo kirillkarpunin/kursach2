4a2a736194b78e1aaa12e6aa341c36c81b931033
Karpunin_Kirill <kirill.karpunin20014@gmail.com>
2023-05-15 00:22:50 +0300

    finalfinal

--- CLI/options_parser/count_sep.c
+++ CLI/options_parser/count_sep.c
@@ -1,6 +1,7 @@
 #include "options_parser.h"
 
 int count_sep(char* str){
+    if (str == NULL) return 0;
     int ret_val = 0;
     for (int i = 0; str[i]; i++){
         if (str[i] == ',') ret_val++;
